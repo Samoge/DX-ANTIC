610b97abec784e8fa1590cf05e9404a12b9d8e69
Samoge <samuelokwuchukwu4@gmail.com>
2022-10-24 14:27:20 +0100

    updated

---
+++ main.js
@@ -0,0 +1,8 @@
+var hamburger = document.querySelector('.hamburgermenu')
+var showlinks = document.querySelector('.actionbuttons')
+// var backdrop = document.querySelector('.backdrop')
+
+function Addclass() {
+    hamburger.classList.toggle('show')
+    showlinks.classList.toggle('smoothmenushow')
+}
