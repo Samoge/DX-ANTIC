fa72c74475d127a7b80919bc69fcf69dae14c356
Samoge <samuelokwuchukwu4@gmail.com>
2022-10-29 21:24:27 +0100

    Page 3

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-var hamburger = document.querySelector('.hamburgermenu')
-var showlinks = document.querySelector('.actionbuttons')
+var hamburger = document.querySelector(".hamburgermenu");
+var showlinks = document.querySelector(".actionbuttons");
 // var backdrop = document.querySelector('.backdrop')
 
 function Addclass() {
-    hamburger.classList.toggle('show')
-    showlinks.classList.toggle('smoothmenushow')
+  hamburger.classList.toggle("show");
+  showlinks.classList.toggle("smoothmenushow");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/Prev controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+  slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {slides[i].style.display = "none";}
+  slides[slideIndex-1].style.display = "block";
 }
